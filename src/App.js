f5c9893052bb3eec5406d1bb928c242e2246be32
Shafee <shafeelinks@gmail.com>
2021-01-20 20:01:28 -0500

    functional. needs design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FruitContainer from "./components/FruitContainer";
+import fruitList from "./components/Data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<FruitContainer fruits={fruitList} />
+		</div>
+	);
 }
 
 export default App;
